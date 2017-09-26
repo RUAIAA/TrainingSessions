5819afbaac2907ed78a99e05595e5a5b9a0d44ba
ethmur <etmuop@gmail.com>
2017-09-26 14:33:42 -0400

    Get rid of absolute path

--- ImageServer/image_server/settings.py
+++ ImageServer/image_server/settings.py
@@ -102,5 +102,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = 'C:/Users/Emurada/Documents/Code/Python/Python34/DjangoTesting/ImageServer/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
